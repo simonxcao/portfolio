28846e61bd26c222042d79a282729e902c4c4add
Simon Cao <96557219+simonxcao@users.noreply.github.com>
2025-01-17 04:20:09 -0800

    fixed contact form

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "tcao10@uci.edu.com",
+      to: "tcao10@uci.edu",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
